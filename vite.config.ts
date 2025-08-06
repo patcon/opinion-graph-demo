532744a8251ae5fc0e2bac455dda8e8ebb6d0533
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-08-06 18:04:52 -0400

    Get vite working properly in ts mode.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode }) => {
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     // This is set in the deploy.yml workflow. Otherwise, no subpath.
     base: env.VITE_GITHUB_REPO_NAME ? `/${env.VITE_GITHUB_REPO_NAME}/` : '',
     plugins: [react()],
-  }
+  };
 });
